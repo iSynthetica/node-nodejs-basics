e72426823dbe8dfd5711db277c5db1fdbe547b4b
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-16 22:40:05 +0300

    feat(fs): implement list function body

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { __filename, __dirname } from './helpers.js';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const fileList = await fs.readdir(path.resolve(__dirname, 'files'));
+
+        if (fileList && fileList.length) {
+            for (let fileName of fileList) console.log(fileName);
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
