196b3db12b6cb3a0d179715066316b6c86ab78b7
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-16 14:08:36 +0300

    feat(fs): implement create function body

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.resolve(path.dirname(__filename), 'files', 'fresh.txt');
+
+    try {
+        await fs.promises.writeFile(filePath, 'I am fresh and young', { flag: 'ax+' });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
