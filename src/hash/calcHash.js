d27f3897dc94b1380db4b3b7c1fe38c5423cee44
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-18 21:47:38 +0300

    feat(hash): add body of calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileBuffer = await fs.readFile(path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    const hash = crypto.createHash('sha256').update(fileBuffer);
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
