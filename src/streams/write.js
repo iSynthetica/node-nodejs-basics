7964a71d3884740cbfcfa998f043b79be6402857
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-18 22:20:18 +0300

    feat(streams): add body of write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'));
+
+    process.stdin.on('data', data => {
+        writeStream.write(data.toString().trim() + '\n');
+    });
 };
 
 await write();
