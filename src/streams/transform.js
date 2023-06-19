d793d63c8add23207ea47d2adacbd32114a59edc
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-19 09:47:52 +0300

    feat(streams): add body of transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+
+const transformInput = new Transform({
+    transform(chunk, encoding, callback){
+        const reversed = chunk.toString().trim().split('').reverse().join('');
+        this.push(chunk.toString().trim().split('').reverse().join('') + '\n');
+        callback();
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(transformInput).pipe(process.stdout);
 };
 
 await transform();
