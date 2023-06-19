c5ecdf45a220f4efe75bd02e56f2ea5264c985c0
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-19 10:21:13 +0300

    feat(zip): add body of compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    await pipeline(
+        createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt')),
+        createGzip(),
+        createWriteStream(path.join(__dirname, 'files', 'archive.gz'))
+    );
 };
 
 await compress();
