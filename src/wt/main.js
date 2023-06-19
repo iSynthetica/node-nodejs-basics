701e39edac788012c2a88335aca6618db6d3914c
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-19 12:23:45 +0300

    feat(wt): create workers from worker.js send data and get result

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker, isMainThread } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        let workersNum = os.cpus().length;
+        let dataArray = Array.from({ length: workersNum }, (_, i) => i + 10);
+        let resultArray = Array.from({ length: workersNum }, (_) => ({status:'pending', result: null}));
+
+        for (let i in dataArray) {
+            const worker = new Worker('./worker.js', { workerData: dataArray[i] });
+
+            worker.on('message', message => {
+                resultArray[i].status = 'resolved';
+                resultArray[i].result = message;
+            });
+
+            worker.on('error', () => {
+                resultArray[i].status = 'error';
+            });
+
+            worker.on('exit', () => {
+                workersNum--;
+
+                if (workersNum === 0) {
+                    console.log(resultArray);
+                }
+            });
+        }
+    } else {
+
+    }
 };
 
 await performCalculations();
