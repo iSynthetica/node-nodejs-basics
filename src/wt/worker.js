f18adc9226d1c14a61fa505f334c2fa45f5eca16
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-19 11:21:18 +0300

    feat(wt): receive n from main, send result to main

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import { fileURLToPath } from 'url';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const __filename = fileURLToPath(import.meta.url);
+
+// n should be received from main thread
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+const sendResult = result => {
+    parentPort.postMessage(result);
 };
 
-sendResult();
+if (isMainThread) {
+    const worker = new Worker(__filename, { workerData: 10 });
+    worker.on('message', (message) => {
+        console.log('nthFibonacci result:', message);
+    })
+} else {
+    sendResult(nthFibonacci(workerData));
+}
